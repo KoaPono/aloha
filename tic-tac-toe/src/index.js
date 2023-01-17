fa1790a6a3e8194ed43d6513a4cf7b88ad654d42
Kelii Deleon <>
2023-01-17 15:46:03 -0700

    Added CalculateWinner Function

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -79,6 +79,26 @@ class Game extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && sqaures[a] === sqaures[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
