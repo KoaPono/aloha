788f3bb89b9f25744046e76a11bc256be02c7a9d
Koa Pono <kp@pridesix.com>
2023-02-15 01:38:09 +0000

    Working on test

--- src/Utils.test.ts
+++ src/Utils.test.ts
@@ -2,19 +2,49 @@ import SquareStates from './SquareState';
 import calculateGameState from './Utils';
 
 describe('calculateGameState', () => {
-  it('should return X when X wins', () => {
-    const squares: Array<SquareStates> = ['X', 'X', 'X', 'O', 'O', '', '', '', ''];
-    expect(calculateGameState(squares)).toBe('X');
+  describe('X wins', () => {
     // TODO: Calculate and test all possible winning combinations for X
+    // 3 across, 3 down, 2 diagonal
+    it('Top across', () => {
+      const squares: Array<SquareStates> = ['X', 'X', 'X', 'O', 'O', '', '', '', ''];
+      expect(calculateGameState(squares)).toBe('X Wins!');
+    })
+    it('Middle across', () => {
+      const squares: Array<SquareStates> = ['O', 'O', '', 'X', 'X', 'X', '', '', ''];
+      expect(calculateGameState(squares)).toBe('X Wins!');
+    })
+    it('Bottom across', () => {
+      const squares: Array<SquareStates> = ['O', 'O', '', '', '', '', 'X', 'X', 'X'];
+      expect(calculateGameState(squares)).toBe('X Wins!');
+    })
+
+    it('Left down', () => {
+
+    })
+    it('Middle down', () => {
+      
+    })
+    it('Right down', () => {
+      
+    })
+
+    it('Left diagonal', () => {
+
+    })
+    it('Middle diagonal', () => {
+      
+    })
+
   });
   it('should return O when O wins', () => {
-    const squares: Array<SquareStates> = ['X', 'X', 'O', 'O', 'O', '', '', '', ''];
-    expect(calculateGameState(squares)).toBe('O');
+    const squares: Array<SquareStates> = ['X', 'X', '', 'O', 'O', 'O', '', '', ''];
+    expect(calculateGameState(squares)).toBe('O Wins!');
     // TODO: Calculate and test all possible winning combinations for O
+    // 3 across, 3 down, 2 diagonal
   });
   it('should return an empty string when no one wins', () => {
-    const squares: Array<SquareStates> = ['X', 'X', 'O', 'O', 'X', '', '', '', ''];
-    expect(calculateGameState(squares)).toBe('');
-    // TODO: Calculate and test all possible draw combinations for 
+    //const squares: Array<SquareStates> = ['X', 'X', 'O', 'O', 'X', '', '', '', ''];
+    //expect(calculateGameState(squares)).toBe('');
+    // TODO: Calculate and test all possible draw combinations
   });
 });
